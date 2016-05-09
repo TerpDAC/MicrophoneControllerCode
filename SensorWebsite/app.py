062dde2622bbcd1eb60817927433afb815973f69
Albert Huang <alberthdev@users.noreply.github.com>
2016-05-09 18:15:42 -0400

    Updated website code from PythonAnywhere

--- SensorWebsite/app.py
+++ SensorWebsite/app.py
@@ -23,26 +23,45 @@ def floor():
 @app.route('/floorstatus')
 def floorstatus():
     #return ",".join(["low", ["low", "med", "high"][random.randrange(0,3)]] + (["low"]*5))
-    
+
     if len(curdata) == 0:
         return ",".join(["low"] * 7)
     else:
         e = curdata[-1]
         total = e[1] + e[2] + e[3]
-        if ((e[3] * 100) / total) >= 75:
+        #if ((e[3] * 100) / total) >= 47:
+        if (total - e[3]) / total >= 50:
             stat = "low"
         else:
             total_no_low = total - e[3]
-            if ((e[1] * 100) / total_no_low) >= 50:
+            if ((e[1] * 100) / total_no_low) >= 25:
                 stat = "high"
             else:
                 stat = "med"
     return ",".join(["low", stat] + (["low"]*5))
 
+@app.route('/floorstatus-dbg')
+def floorstatusdbg():
+    if len(curdata) == 0:
+        return "No data!"
+    else:
+        e = curdata[-1]
+        total = e[1] + e[2] + e[3]
+
+        total_no_low = total - e[3]
+
+        low_percent = ((e[3] * 100) / total)
+        med_percent = ((e[2] * 100) / total)
+        high_percent = ((e[1] * 100) / total)
+        high_percent_in_nonlow = ((e[1] * 100) / total_no_low)
+        med_percent_in_nonlow = ((e[2] * 100) / total_no_low)
+
+        return "Low percent: %.2f%% | Med percent: %.2f%% | High percent: %.2f%% | Med percent (within med/high): %.2f%% | High percent (within med/high): %.2f%%" % (low_percent, med_percent, high_percent, med_percent_in_nonlow, high_percent_in_nonlow)
+
 @app.route('/rawdata')
 def rawdata():
     output = "Date,High,Med,Low\n"
-    
+
     for e in curdata:
         print(e)
         print(datetime.datetime.fromtimestamp(e[0]).strftime("%Y/%m/%d %H:%M:%S"))
@@ -52,30 +71,30 @@ def rawdata():
         buf.append(str(e[2]))
         buf.append(str(e[3]))
         output += ",".join(buf) + "\n"
-     
+
     return output
 
 @app.route('/datasubmit')
 def dataSubmit():
     d = request.args.get('d', None)
-    
+
     if not d:
         return "ERROR"
-    
+
     data = d.split(",")
-    
+
     print("INPUT INCOMING")
     print(d)
     print(data)
-    
+
     timestamp = int(data[0])
     high = int(data[1])
     med = int(data[2])
     low = int(data[3])
-    
+
     new_data = [timestamp, high, med, low]
     curdata.append(list(new_data))
-    
+
     return "OK"
 
 if __name__ == '__main__':
--- SensorWebsite/static/js/floors.js
+++ SensorWebsite/static/js/floors.js
@@ -1,61 +1,65 @@
 var curFloorStatus = "";
 
 function updateFloorStatus() {
+    $.get("floorstatus-dbg", function(dbgStatusString) {
+        $('#dbgstatus').html(dbgStatusString);
+    });
+
     $.get("floorstatus", function(statusString) {
         floorStatuses = statusString.split(",");
         if (floorStatuses.length >= 2) {
             newFloorStatus = floorStatuses[1];
-            
+
             if (newFloorStatus != curFloorStatus) {
                 $('#floor2').removeClass("waves-green");
                 $('#floor2').removeClass("waves-yellow");
                 $('#floor2').removeClass("waves-red");
-                
+
                 if (newFloorStatus == "low") {
                     curClasses = $('#floor2').prop('class');
-                    
+
                     $('#floor2').addClass("waves-green");
                     Waves.ripple('#floor2');
-                    
+
                     if (curClasses.search("high") != -1) {
                         $('#floor2').switchClass("high", "low", 1000, "easeInOutQuad");
                     } else {
                         $('#floor2').switchClass("med", "low", 1000, "easeInOutQuad");
                     }
-                    
+
                     $('#floor2 div:nth-child(2)').html('<i class="fa fa-volume-off"></i>');
                 }
-                
+
                 if (newFloorStatus == "med") {
                     curClasses = $('#floor2').prop('class');
-                    
+
                     $('#floor2').addClass("waves-yellow");
                     Waves.ripple('#floor2');
-                    
+
                     if (curClasses.search("high") != -1) {
                         $('#floor2').switchClass("high", "med", 1000, "easeInOutQuad");
                     } else {
                         $('#floor2').switchClass("low", "med", 1000, "easeInOutQuad");
                     }
-                    
+
                     $('#floor2 div:nth-child(2)').html('<i class="fa fa-volume-down"></i>');
                 }
-                
+
                 if (newFloorStatus == "high") {
                     curClasses = $('#floor2').prop('class');
-                    
+
                     $('#floor2').addClass("waves-red");
                     Waves.ripple('#floor2');
-                    
+
                     if (curClasses.search("med") != -1) {
                         $('#floor2').switchClass("med", "high", 1000, "easeInOutQuad");
                     } else {
                         $('#floor2').switchClass("low", "high", 1000, "easeInOutQuad");
                     }
-                    
+
                     $('#floor2 div:nth-child(2)').html('<i class="fa fa-volume-up"></i>');
                 }
-                
+
                 curFloorStatus = newFloorStatus;
             }
         }
