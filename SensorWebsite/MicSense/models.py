6ab2b0e4b887a4650a0132285d6aa3b340346d87
Benjamin Haley <Benjamin Haley>
2016-12-09 12:38:30 -0500

    Implemented a one to many relationship with backref and lazy

--- SensorWebsite/MicSense/models.py
+++ SensorWebsite/MicSense/models.py
@@ -5,12 +5,12 @@ from MicSense.db import db, initDatabase
 class Data(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
-    #stores the mac address, no colons
-    mac_address = db.Column(db.String(12))
     timestamp = db.Column(db.DateTime())
     high = db.Column(db.Integer())
     med = db.Column(db.Integer())
     low = db.Column(db.Integer())
+    #Foreign key referencing Sensor
+    mac_address = db.Column(db.String(12), ForeignKey='sensor.mac_address')
 
     #name = db.Column(db.String(80), unique=True)
     #description = db.Column(db.String(255))
@@ -20,6 +20,8 @@ class Sensor(db.Model):
     id = db.Column(db.Integer(), primary_key=True)    
     floor_num = db.Column(db.Integer())
     location = db.Column(db.String(100))
-    mac_address = db.Column(db.String(12), db.ForeignKey('data.mac_address'))
+    mac_address = db.Column(db.String(12), unique=True)
+    #initializes a one to many relationship with data
+    data = db.relationship('Data', backref='sensor', lazy='dynamic')
     
 initDatabase()
