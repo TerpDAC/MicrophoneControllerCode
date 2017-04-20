a4b41f06cb04fa8ad493bbede05da361dc2c3db8
Ben Haley <bhaley7@terpmail.umd.edu>
2017-04-20 18:17:53 -0400

    Made it work

--- SensorWebsite/run_micsense.py
+++ SensorWebsite/run_micsense.py
@@ -1,3 +1,11 @@
+import traceback
 # Run a test server.
-from MicSense import app
-app.run(host='0.0.0.0', port=8080, debug=True)
+
+try:
+    print("Hello")
+    from MicSense import app
+    app.run(host='localhost', port=8080, debug=True)
+    input("Press ENTER: ")
+except:
+    traceback.print_exc()
+    input("Press ENTER: ")
